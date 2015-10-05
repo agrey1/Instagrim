7b866d565a5ce4a283a0a684553c5919dc7a4998
agrey1 <alexandergrey@hotmail.co.uk>
2015-10-05 17:20:39 +0100

    Added log out function

---
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/LogOut.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.dundee.computing.aec.instagrim.servlets;
+
+import com.datastax.driver.core.Cluster;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import uk.ac.dundee.computing.aec.instagrim.lib.CassandraHosts;
+import uk.ac.dundee.computing.aec.instagrim.models.User;
+import uk.ac.dundee.computing.aec.instagrim.stores.LoggedIn;
+
+/**
+ *
+ * @author AlexanderGrey
+ */
+@WebServlet(name = "LogOut", urlPatterns = {"/LogOut"})
+public class LogOut extends HttpServlet 
+{
+    /**
+     * Processes requests for both HTTP <code>GET</code>
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        HttpSession session = request.getSession();
+        LoggedIn lg = (LoggedIn) session.getAttribute("LoggedIn");
+        if (lg != null)
+        {
+            if (lg.getlogedin())
+            {
+                session.removeAttribute("LoggedIn");
+            }
+        }
+        
+        response.setContentType("text/html;charset=UTF-8");
+        response.sendRedirect("/Instagrim/logout.jsp");
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        processRequest(request, response);
+    }
+    
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() 
+    {
+        return "Short description";
+    }// </editor-fold>
+}
---
+++ target/Instagrim-1.0-SNAPSHOT/logout.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : logout
+    Created on : 05-Oct-2015, 15:32:18
+    Author     : Area 51
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <p>You have been logged out. Click <a href="/">here</a> to return to the home page.</p>
+    </body>
+</html>
